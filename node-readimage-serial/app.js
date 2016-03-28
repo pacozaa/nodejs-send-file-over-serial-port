0a0da173195336d79050b56478c934af6002f50a
pacozaa <pacosarin@gmail.com>
2016-03-28 20:22:53 +0700

    use binary option in writefile

--- node-readimage-serial/app.js
+++ node-readimage-serial/app.js
@@ -1,7 +1,7 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
 serialPort = new SerialPort("COM7", {
-  baudrate: 9600,
+  baudrate: 1200,
   parser: serialport.parsers.readline("close")
 });
 var fs = require('fs');
@@ -9,7 +9,7 @@ serialPort.on("open", function () {
   console.log('open');
   serialPort.on('data', function(data) {
     console.log('data received: ',data);
-    fs.writeFile('test.txt', data, function(err) {
+    fs.writeFile('test7.bmp', data ,'binary', function(err) {
         if(err) {
             return console.log('file save error : ',err);
         }
