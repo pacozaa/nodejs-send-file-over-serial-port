095c7755abc5ae79f9d4c851d5311c80faefc7d0
pacozaa <pacosarin@gmail.com>
2016-07-24 17:09:43 +0700

    finish assembling image waiting for test :)

--- node-readimage-serial/app.js
+++ node-readimage-serial/app.js
@@ -1,19 +1,30 @@
-var serialport = require("serialport");
+var serialport = require('serialport');
 var SerialPort = serialport.SerialPort; // localize object constructor
-serialPort = new SerialPort("COM7", {
+var chunks = [];
+
+serialPort = new SerialPort('COM5', {
   baudrate: 1200,
-  parser: serialport.parsers.readline("close")
+  parser: serialport.parsers.raw,
 });
 var fs = require('fs');
-serialPort.on("open", function () {
+serialPort.on('open', function () {
   console.log('open');
   serialPort.on('data', function(data) {
-    console.log('data received: ',data);
-    fs.writeFile('test7.bmp', data ,'binary', function(err) {
-        if(err) {
-            return console.log('file save error : ',err);
-        }
-        console.log("The file was saved!");
-    });
+    if(data.indexOf("close") > 0){
+      const file =  Buffer.concat(chunks);
+      console.log(file.length);
+      fs.writeFile('1', file ,'binary', function(err) {
+          if(err) {
+              return console.log('file save error : ',err);
+          }
+      });
+    }
+    else {
+     chunks.push(data);
+    }
   });
+});
+
+serialPort.on('error', function() {
+  console.log('error');
 });
